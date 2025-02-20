174fa53fb659b8bc7bcd7fd8e984361203780690
PaClimaco <climacopaolo@gmail.com>
2025-02-20 14:29:29 +0100

    update docs

---
+++ docs/source/tutorials/utils_tutorial/__init__.py
@@ -0,0 +1,7 @@
+from .plots import  generate_barplots, generate_lineplots, generate_lineplots_analysis
+
+__all__ = [
+        'generate_barplots',
+        'generate_lineplots', 
+        'generate_lineplots_analysis', 
+        ]
